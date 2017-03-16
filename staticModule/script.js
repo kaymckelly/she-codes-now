d08250fe6407ee1412405999bf214e8030bfd9bb
Kay McKelly <frecklestars@gmail.com>
2017-03-15 17:24:59 -0700

    Added first two examples

---
+++ staticModule/script.js
@@ -0,0 +1,14 @@
+(function (angular) {
+  'use strict';
+
+  // declare a module
+  var myAppModule = angular.module('myApp', []);
+
+  // configure the module
+  myAppModule.filter('greet', function() {
+    return function(name) {
+      return 'Hello, ' + name + '!';
+    };
+  });
+  
+})(window.angular);
